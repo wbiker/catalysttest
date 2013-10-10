323c9cd8d888df796ac75b8745cda026a4a4a69d
wolf <wbiker@gmx.at>
2013-10-10 19:34:39 +0200

    Initalize check-in

---
+++ makeTests.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: makeTests.pl
+#
+#        USAGE: ./makeTests.pl  
+#
+#  DESCRIPTION: Creates a lot of test runs 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: wba (wolf), wbiker@gmx.at
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 10/10/13 19:32:52
+#     REVISION: ---
+#===============================================================================
+
+use Modern::Perl;
+
+use Data::Dump::Streamer;
+
+
